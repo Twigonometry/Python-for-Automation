f5c9412c32acda8ef421dc6f80c09cbc9af86d2a
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2020-07-28 21:32:47 +0100

    Scraping tags

--- Section 2 - Scraping/scrape.py
+++ Section 2 - Scraping/scrape.py
@@ -11,5 +11,12 @@ quotes = soup.find_all('span', class_='text')
 
 authors = soup.find_all('small', class_='author')
 
+tags = soup.find_all('div', class_='tags')
+
 for i in range(len(quotes)):
     print(quotes[i].text + " - " + authors[i].text)
+    quote_tags = tags[i].find_all('a', class_='tag')
+    print("Tags:")
+    for tag in quote_tags:
+        print(tag.text)
+    print()
