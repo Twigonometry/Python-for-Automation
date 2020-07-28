8cd5533fe2fa2e92b0d671eda95b07f03c0e49ae
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2020-07-28 22:35:53 +0100

    Basic JSON parsing

--- Section 4 - APIs/api_call.py
+++ Section 4 - APIs/api_call.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 base_url = "https://api.upcitemdb.com/prod/trial/lookup"
 
@@ -7,3 +8,14 @@ parameters = {'upc': '012993441012'}
 response = requests.get(base_url, params=parameters)
 
 print(response.url)
+
+content = response.content
+response_dict = json.loads(content)
+print(response_dict)
+
+iteminfo = response_dict['items'][0]
+title = iteminfo['title']
+brand = iteminfo['brand']
+
+print("Title: " + title)
+print("Brand: " + brand)
