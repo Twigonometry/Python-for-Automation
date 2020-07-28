7a46a1f220b88684f11d5d41806f5c014c4faf3f
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2020-07-28 21:57:23 +0100

    Paginated scraping

--- Section 2 - Scraping/scrape_pages.py
+++ Section 2 - Scraping/scrape_pages.py
@@ -7,15 +7,20 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'lxml')
 
-items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+def scrape_page(page_url, count):
+    response = requests.get(page_url)
 
-count = 1
+    page_soup = BeautifulSoup(response.text, 'lxml')
 
-for i in items:
-    item_name = i.find('h4', class_='card-title').text.strip('\n')
-    item_price = i.find('h5').text
-    print('%s) Price: %s, Item Name: %s' % (count, item_price, item_name))
-    count += 1
+    items = page_soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+
+    for i in items:
+        item_name = i.find('h4', class_='card-title').text.strip('\n')
+        item_price = i.find('h5').text
+        print('%s) Price: %s, Item Name: %s' % (count, item_price, item_name))
+        count += 1
+
+    return count
 
 pages = soup.find('ul', class_="pagination")
 urls = []
@@ -24,6 +29,10 @@ for link in links:
     page_num = int(link.text) if link.text.isdigit() else None
     if page_num != None:
         x = link.get('href')
-        urls.append(x)
+        urls.append("https://scrapingclub.com/exercise/list_basic/" + x)
 
-print(urls)
+count = 1
+
+for url in urls:
+    new_items = scrape_page(url, count)
+    count = new_items
