f9cf3cd44d42311930d9a5839ab5594e74ff5bcc
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2020-07-28 20:32:52 +0100

    Outputting failures

--- Section 1/file_manipulation.py
+++ Section 1/file_manipulation.py
@@ -1,6 +1,7 @@
 f = open('../Ex_Files_Python_Automation/Exercise Files/inputFile.txt','r')
 
 pass_file = open('passfile.txt','w')
+fail_file = open('failfile.txt','w')
 
 count = 0
 
@@ -8,6 +9,9 @@ for line in f:
     line_split = line.split()
     if line_split[2] == 'P':
         pass_file.write(line)
+    else:
+        fail_file.write(line)
 
 f.close()
 pass_file.close()
+fail_file.close()
