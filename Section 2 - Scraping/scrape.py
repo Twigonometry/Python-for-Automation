8f3fe115dac425e800e327cfeb06810004c2b75a
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2020-07-28 21:26:05 +0100

    Scraping just quotes

--- Section 2 - Scraping/scrape.py
+++ Section 2 - Scraping/scrape.py
@@ -7,4 +7,7 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'lxml')
 
-print(soup)
+quotes = soup.find_all('span', class_='text')
+
+for quote in quotes:
+    print(quote.text)
